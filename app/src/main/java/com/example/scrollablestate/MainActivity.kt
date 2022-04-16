d3cf580355e87d23f104e9fa0ad484769504954c
Joel-K-Muraguri <joelkmuraguri21@gmail.com>
2022-04-16 23:04:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/scrollablestate/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.scrollablestate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.scrollablestate.ui.theme.ScrollableStateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScrollableStateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+
+    var scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .verticalScroll(state = scrollState)
+            .background(Color.Cyan)
+    ) {
+        for(i in 1..50){
+            Text(
+                text = "Items $i",
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp)
+                    .background(Color.Cyan),
+                fontWeight = FontWeight.ExtraBold,
+                textAlign = TextAlign.Center,
+
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ScrollableStateTheme {
+        Greeting()
+    }
+}
